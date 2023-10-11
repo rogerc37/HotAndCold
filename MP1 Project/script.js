b1727ebfbab947fca63310ea7f96f24c1499d6a2
rogerc37 <145470030+rogerc37@users.noreply.github.com>
2023-10-11 10:57:09 -0400

    Add files via upload

--- MP1 Project/script.js
+++ MP1 Project/script.js
@@ -1,7 +1,13 @@
 const minmax = document.getElementById("minmax");
+const makeguess = document.getElementById("makeguess");
+const temp = document.getElementById("temp");
 minmax.addEventListener('click', setBoundaries);
+makeguess.addEventListener('click', guess);
 var min = 0;
 var max = 0;
+var guess = 0;
+var answer = 0;
+var range = 0;
 function setBoundaries()
 {
     const inputmin = prompt("Enter the minimum number in your range.");
@@ -20,7 +26,49 @@ function setBoundaries()
     //else{
         max = (inputmax);
     //}
+    range = max - min;
+    answer = (Math.floor((Math.random()*max - min + 1) + min));
     minmax.innerHTML = "Minimum: " + min + " Maximum: " + max;
+}
+function guess()
+{
+    const inputguess = prompt("Put in your guess.");
+    if (guess == answer)
+    {
+        temp.textContent = "YOU WIN"
+    }
+    if(Math.abs((inputguess-answer)) <= (0.02*range))
+    {
+        temp.textContent = "Status: Boiling";
+    }
+    else if(Math.abs((inputguess-answer)) <= (0.05*range))
+    {
+        temp.textContent = "Status: Burning";
+    }
+    else if(Math.abs((inputguess-answer)) <= (0.1*range))
+    {
+        temp.textContent = "Status: Hot";
+    }
+    else if(Math.abs((inputguess-answer)) <= (0.25*range))
+    {
+        temp.textContent = "Status: Warm";
+    }
+    else if(Math.abs((inputguess-answer)) <= (0.5*range))
+    {
+        temp.textContent = "Status: Chilly";
+    }
+    else if(Math.abs((inputguess-answer)) <= (0.75*range))
+    {
+        temp.textContent = "Status: Cold";
+    }
+    else
+    {
+        temp.textContent = "Status: Freezing";
+    }
+    if((guess < min) || (guess > max))
+    {
+        temp.textContent = "Status; Invalid"
+    }
 }
 
 
